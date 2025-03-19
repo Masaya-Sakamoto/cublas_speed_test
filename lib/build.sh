0d84daa3256ec352957162b60f4584eaf39fbe73
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-03-19 12:32:20 +0900

    ビルドスクリプト変更

--- lib/build.sh
+++ lib/build.sh
@@ -6,20 +6,20 @@ CXX_FLAGS=" -L/usr/local/lib -lblis-mt -I/usr/local/include/blis"
 
 #clean up
 rm -rf build
-mkdir build
+mkdir -p build/modules
 
 # compile utils
-g++ -Iinclude -c src/utils.cpp -o build/utils.o
+g++ -Iinclude -c src/utils.cpp -o build/modules/utils.o
 
 # compile CXX files
-g++ -Iinclude -c src/cblas_col_speed_test.cpp -o build/cblas_col_speed_test.o -std=c++20
-g++ -Iinclude -c src/cblas_row_speed_test.cpp -o build/cblas_row_speed_test.o -std=c++20
+g++ -Iinclude -c src/cblas_col_speed_test.cpp -o build/modules/cblas_col_speed_test.o -std=c++20
+g++ -Iinclude -c src/cblas_row_speed_test.cpp -o build/modules/cblas_row_speed_test.o -std=c++20
 
 # link cxx objects
-g++ build/cblas_col_speed_test.o build/utils.o -o build/cblas_col_speed_test $CXX_FLAGS
-g++ build/cblas_row_speed_test.o build/utils.o -o build/cblas_row_speed_test $CXX_FLAGS
+g++ build/modules/cblas_col_speed_test.o build/modules/utils.o -o build/cblas_col_speed_test $CXX_FLAGS
+g++ build/modules/cblas_row_speed_test.o build/modules/utils.o -o build/cblas_row_speed_test $CXX_FLAGS
 
 # compile CUDA file
-nvcc -Iinclude -c src/cublas_speed_test.cu -o build/cublas_speed_test.o $CXX_FLAGS
+nvcc -Iinclude -c src/cublas_speed_test.cu -o build/modules/cublas_speed_test.o
 # link cuda and cxx objects
-nvcc build/cublas_speed_test.o build/utils.o -o build/cublas_speed_test $CXX_FLAGS -lcublas
+nvcc build/modules/cublas_speed_test.o build/modules/utils.o -o build/cublas_speed_test $CXX_FLAGS -lcublas
