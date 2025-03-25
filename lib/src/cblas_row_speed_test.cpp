9b23c6618604d8d224eb1378b2aa288b63335725
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-03-25 12:19:56 +0900

    Add warmup run to speed test iterations; skip first measurement for accurate results

--- lib/src/cblas_row_speed_test.cpp
+++ lib/src/cblas_row_speed_test.cpp
@@ -29,14 +29,17 @@ int main(int argc, char *argv[])
     std::vector<double> memcpy_h2d_results(iters, 0.0);
     std::vector<double> memcpy_d2h_results(iters, 0.0);
 
-    for (int i = 0; i < iters; i++)
+    for (int i = 0; i < iters+1; i++) // iters + warmup run
     {
         setArrays(A, B, C, &alpha, &beta, M, N, K);
         auto start = std::chrono::high_resolution_clock::now();
         cblas_cgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, &alpha, A, K, B, N, &beta, C, N);
         auto cpu_duration = std::chrono::high_resolution_clock::now() - start;
         auto time_count = (double)std::chrono::duration_cast<std::chrono::microseconds>(cpu_duration).count() / 1000;
-        ms_results.push_back(time_count);
+        if (i != 0)
+        {
+            ms_results.push_back(time_count);
+        }
     }
     printResults(ms_results, memcpy_h2d_results, memcpy_d2h_results);
 
