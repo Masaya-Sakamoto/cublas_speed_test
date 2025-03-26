1a37ea12380c20d21370681d5a12c633d537b9f2
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-03-26 17:51:34 +0900

    Increase N_critical and N_limit values & optimize compilation flags
    - Updated N_critical and N_limit values from 12880 to 122880 in
       and  for consistency.
    - Removed debug flags (-g, -O0) and applied optimization level -O3
      in  to improve performance.

--- lib/build.sh
+++ lib/build.sh
@@ -9,17 +9,17 @@ rm -rf build
 mkdir -p build/modules
 
 # compile utils
-g++ -g -O3 -Iinclude -c src/utils.cpp -o build/modules/utils.o
+g++ -O3 -Iinclude -c src/utils.cpp -o build/modules/utils.o
 
 # compile CXX files
-g++ -g -O0 -Iinclude -c src/cblas_col_speed_test.cpp -o build/modules/cblas_col_speed_test.o -std=c++20
-g++ -g -O0 -Iinclude -c src/cblas_row_speed_test.cpp -o build/modules/cblas_row_speed_test.o -std=c++20
+g++ -O3 -Iinclude -c src/cblas_col_speed_test.cpp -o build/modules/cblas_col_speed_test.o -std=c++20
+g++ -O3 -Iinclude -c src/cblas_row_speed_test.cpp -o build/modules/cblas_row_speed_test.o -std=c++20
 
 # link cxx objects
-g++ -g build/modules/cblas_col_speed_test.o build/modules/utils.o -o build/cblas_col_speed_test $CXX_FLAGS
-g++ -g build/modules/cblas_row_speed_test.o build/modules/utils.o -o build/cblas_row_speed_test $CXX_FLAGS
+g++ build/modules/cblas_col_speed_test.o build/modules/utils.o -o build/cblas_col_speed_test $CXX_FLAGS
+g++ build/modules/cblas_row_speed_test.o build/modules/utils.o -o build/cblas_row_speed_test $CXX_FLAGS
 
 # compile CUDA file
-nvcc -g -O0 -Iinclude -c src/cublas_speed_test.cu -o build/modules/cublas_speed_test.o
+nvcc -O3 -Iinclude -c src/cublas_speed_test.cu -o build/modules/cublas_speed_test.o
 # link cuda and cxx objects
-nvcc -g build/modules/cublas_speed_test.o build/modules/utils.o -o build/cublas_speed_test $CXX_FLAGS -lcublas
+nvcc build/modules/cublas_speed_test.o build/modules/utils.o -o build/cublas_speed_test $CXX_FLAGS -lcublas
